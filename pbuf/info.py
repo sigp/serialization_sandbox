3a11413ff4e1348d46fff31a9c6a9fec6ab52115
NatoliChris <natolichris@gmail.com>
2018-08-21 13:07:05 +1000

    Standardise protobuf tests

--- pbuf/info.py
+++ pbuf/info.py
@@ -3,6 +3,9 @@ import messages_pb2
 import packedmessages_pb2
 import argparse
 import texttable 
+import sys,os
+sys.path.append(os.path.join(os.path.dirname(sys.path[0]), 'helpers'))
+import helpers
 
 verbose = False
 
@@ -11,31 +14,31 @@ def explain_default_size():
   attestation_record = messages_pb2.AttestationRecord()
   attestation_record.slot = 0
   attestation_record.shard_id = 0
-  attestation_record.shard_block_hash = b'\x00'*32
-  attestation_record.attester_bitfield = b'\x00'*32
+  attestation_record.shard_block_hash = helpers.EMPTY_BYTES
+  attestation_record.attester_bitfield = helpers.EMPTY_BYTES
   attestation_record.aggregate_sig.append(0)
   attestation_record.aggregate_sig.append(0)
 
   block = messages_pb2.Block()
-  block.parent_hash= b'\x00'*32
+  block.parent_hash= helpers.EMPTY_BYTES
   block.slot_number= 0
-  block.randao_reveal= b'\x00'
-  block.pow_chain_ref = b'\x00'*32
-  block.active_state_hash = b'\x00'*32
-  block.crystallized_state_root = b'\x00'*32
+  block.randao_reveal= helpers.EMPTY_BYTES
+  block.pow_chain_ref = helpers.EMPTY_BYTES
+  block.active_state_hash = helpers.EMPTY_BYTES
+  block.crystallized_state_root = helpers.EMPTY_BYTES
 
   shard_and_committee = messages_pb2.ShardAndCommittee()
   shard_and_committee.shard_id = 0
 
   crosslink_record = messages_pb2.CrosslinkRecord()
   crosslink_record.dynasty = 0
-  crosslink_record.blockhash = b'\x00'*32
+  crosslink_record.blockhash = helpers.EMPTY_BYTES
 
   validator_record = messages_pb2.ValidatorRecord()
   validator_record.public_key = 0
   validator_record.withdrawal_shard = 0
-  validator_record.withdrawal_address = b'\x00'*32
-  validator_record.randao_commitment = b'\x00'*32
+  validator_record.withdrawal_address = helpers.EMPTY_BYTES
+  validator_record.randao_commitment = helpers.EMPTY_BYTES
   validator_record.balance = 0
   validator_record.start_dynasty = 0
   validator_record.end_dynasty = 0
@@ -48,7 +51,7 @@ def explain_default_size():
   crystallized_state.current_dynasty = 0
   crystallized_state.crosslinking_start_shard = 0
   crystallized_state.total_deposits = 0
-  crystallized_state.dynasty_seed = b'\x00'*32
+  crystallized_state.dynasty_seed = helpers.EMPTY_BYTES
   crystallized_state.dynasty_seed_last_reset = 0
 
 
@@ -85,31 +88,31 @@ def explain_default_packed():
   attestation_record = packedmessages_pb2.PackedAttestationRecord()
   attestation_record.slot = 0
   attestation_record.shard_id = 0
-  attestation_record.shard_block_hash = b'\x00'*32
-  attestation_record.attester_bitfield = b'\x00'*32
+  attestation_record.shard_block_hash = helpers.EMPTY_BYTES
+  attestation_record.attester_bitfield = helpers.EMPTY_BYTES
   attestation_record.aggregate_sig.append(0)
   attestation_record.aggregate_sig.append(0)
 
   block = packedmessages_pb2.PackedBlock()
-  block.parent_hash= b'\x00'*32
+  block.parent_hash= helpers.EMPTY_BYTES
   block.slot_number= 0
-  block.randao_reveal= b'\x00'
-  block.pow_chain_ref = b'\x00'*32
-  block.active_state_hash = b'\x00'*32
-  block.crystallized_state_root = b'\x00'*32
+  block.randao_reveal= helpers.EMPTY_BYTES
+  block.pow_chain_ref = helpers.EMPTY_BYTES
+  block.active_state_hash = helpers.EMPTY_BYTES
+  block.crystallized_state_root = helpers.EMPTY_BYTES
 
   shard_and_committee = packedmessages_pb2.PackedShardAndCommittee()
   shard_and_committee.shard_id = 0
 
   crosslink_record = packedmessages_pb2.PackedCrosslinkRecord()
   crosslink_record.dynasty = 0
-  crosslink_record.blockhash = b'\x00'*32
+  crosslink_record.blockhash = helpers.EMPTY_BYTES
 
   validator_record = packedmessages_pb2.PackedValidatorRecord()
   validator_record.public_key = 0
   validator_record.withdrawal_shard = 0
-  validator_record.withdrawal_address = b'\x00'*32
-  validator_record.randao_commitment = b'\x00'*32
+  validator_record.withdrawal_address = helpers.EMPTY_BYTES
+  validator_record.randao_commitment = helpers.EMPTY_BYTES
   validator_record.balance = 0
   validator_record.start_dynasty = 0
   validator_record.end_dynasty = 0
@@ -122,7 +125,7 @@ def explain_default_packed():
   crystallized_state.current_dynasty = 0
   crystallized_state.crosslinking_start_shard = 0
   crystallized_state.total_deposits = 0
-  crystallized_state.dynasty_seed = b'\x00'*32
+  crystallized_state.dynasty_seed = helpers.EMPTY_BYTES
   crystallized_state.dynasty_seed_last_reset = 0
 
 
@@ -155,78 +158,83 @@ def explain_max_size():
 
   # Size of struct with only defaults
   attestation_record = messages_pb2.AttestationRecord()
-  attestation_record.slot = 18446744073709551615
-  attestation_record.shard_id = 18446744073709551615
-  attestation_record.shard_block_hash = b'\xff'*32
-  attestation_record.attester_bitfield = b'\xff'*32
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
+  attestation_record.slot = helpers.MAX_U64
+  attestation_record.shard_id = helpers.MAX_U64
+  attestation_record.shard_block_hash = helpers.MAX_BYTES
+  attestation_record.attester_bitfield = helpers.MAX_BYTES
+  for i in range(0, 64):
+    attestation_record.aggregate_sig.append(helpers.MAX_U64)
 
   block = messages_pb2.Block()
-  block.parent_hash= b'\xff'*32
-  block.slot_number= 18446744073709551615
-  block.randao_reveal= b'\xff'
-  block.pow_chain_ref = b'\xff'*32
-  block.active_state_hash = b'\xff'*32
-  block.crystallized_state_root = b'\xff'*32
+  block.parent_hash= helpers.MAX_BYTES
+  block.slot_number= helpers.MAX_U64
+  block.randao_reveal= helpers.MAX_BYTES
+  block.pow_chain_ref = helpers.MAX_BYTES
+  block.active_state_hash = helpers.MAX_BYTES
+  block.crystallized_state_root = helpers.MAX_BYTES
+  for i in range(0, 2000):
+    arecord = block.attestations.add()
+    arecord.slot = helpers.MAX_U64
+    arecord.shard_id = helpers.MAX_U64
+    arecord.shard_block_hash = helpers.MAX_BYTES
+    arecord.attester_bitfield = helpers.MAX_BYTES
+    for i in range(0, 64):
+      arecord.aggregate_sig.append(helpers.MAX_U64)
+
 
   shard_and_committee = messages_pb2.ShardAndCommittee()
-  shard_and_committee.shard_id = 18446744073709551615
+  shard_and_committee.shard_id = helpers.MAX_U64
   for i in range(0, 1000):
-    shard_and_committee.committee.append(4294967295)
+    shard_and_committee.committee.append(helpers.MAX_U32)
 
   crosslink_record = messages_pb2.CrosslinkRecord()
-  crosslink_record.dynasty = 18446744073709551615
-  crosslink_record.blockhash = b'\xff'*32
+  crosslink_record.dynasty = helpers.MAX_U64
+  crosslink_record.blockhash = helpers.MAX_BYTES
 
   validator_record = messages_pb2.ValidatorRecord()
-  validator_record.public_key = 18446744073709551615
-  validator_record.withdrawal_shard = 18446744073709551615
-  validator_record.withdrawal_address = b'\xff'*32
-  validator_record.randao_commitment = b'\xff'*32
-  validator_record.balance = 18446744073709551615
-  validator_record.start_dynasty = 18446744073709551615
-  validator_record.end_dynasty = 18446744073709551615
+  validator_record.public_key = helpers.MAX_U64
+  validator_record.withdrawal_shard = helpers.MAX_U16
+  validator_record.withdrawal_address = helpers.MAX_BYTES
+  validator_record.randao_commitment = helpers.MAX_BYTES
+  validator_record.balance = helpers.MAX_U64
+  validator_record.start_dynasty = helpers.MAX_U64
+  validator_record.end_dynasty = helpers.MAX_U64
 
   crystallized_state = messages_pb2.CrystallizedState()
-  crystallized_state.last_state_recalc = 18446744073709551615
-  crystallized_state.justified_streak = 18446744073709551615
-  crystallized_state.last_justified_slot = 18446744073709551615
-  crystallized_state.last_finalized_slot = 18446744073709551615
-  crystallized_state.current_dynasty = 18446744073709551615
-  crystallized_state.crosslinking_start_shard = 18446744073709551615
-  crystallized_state.total_deposits = 18446744073709551615
-  crystallized_state.dynasty_seed = b'\xff'*32
-  crystallized_state.dynasty_seed_last_reset = 18446744073709551615
+  crystallized_state.last_state_recalc = helpers.MAX_U64
+  crystallized_state.justified_streak = helpers.MAX_U64
+  crystallized_state.last_justified_slot = helpers.MAX_U64
+  crystallized_state.last_finalized_slot = helpers.MAX_U64
+  crystallized_state.current_dynasty = helpers.MAX_U64
+  crystallized_state.crosslinking_start_shard = helpers.MAX_U16
+  crystallized_state.total_deposits = helpers.MAX_U64
+  crystallized_state.dynasty_seed = helpers.MAX_BYTES
+  crystallized_state.dynasty_seed_last_reset = helpers.MAX_U64
 
 
   for i in range(0, 1000):
     crecord = crystallized_state.crosslink_records.add()
-    crecord.dynasty = 18446744073709551615
-    crecord.blockhash = b'\xff'*32
+    crecord.dynasty = helpers.MAX_U64
+    crecord.blockhash = helpers.MAX_BYTES
 
   for i in range(0, 1000):
     vrecord = crystallized_state.validators.add()
-    vrecord.public_key = 18446744073709551615
-    vrecord.withdrawal_shard = 18446744073709551615
-    vrecord.withdrawal_address = b'\xff'*32
-    vrecord.randao_commitment = b'\xff'*32
-    vrecord.balance = 18446744073709551615
-    vrecord.start_dynasty = 18446744073709551615
-    vrecord.end_dynasty = 18446744073709551615
+    vrecord.public_key = helpers.MAX_U64
+    vrecord.withdrawal_shard = helpers.MAX_U64
+    vrecord.withdrawal_address = helpers.MAX_BYTES
+    vrecord.randao_commitment = helpers.MAX_BYTES
+    vrecord.balance = helpers.MAX_U64
+    vrecord.start_dynasty = helpers.MAX_U64
+    vrecord.end_dynasty = helpers.MAX_U64
 
 
   for i in range(0, 1000):
     shard_comittee_array = crystallized_state.indices_for_heights.add()
     for i in range(0, 10):
       scr = shard_comittee_array.array_shard_and_committee.add()
-      scr.shard_id = 18446744073709551615
+      scr.shard_id = helpers.MAX_U64
       for i in range(0, 1000):
-        scr.committee.append(4294967295)
+        scr.committee.append(helpers.MAX_U32)
 
 
   attestation_record_bytes = attestation_record.SerializeToString()
@@ -256,80 +264,87 @@ def explain_max_size():
 
 def explain_max_packed():
 
+
   # Size of struct with only defaults
   attestation_record = packedmessages_pb2.PackedAttestationRecord()
-  attestation_record.slot = 18446744073709551615
-  attestation_record.shard_id = 18446744073709551615
-  attestation_record.shard_block_hash = b'\xff'*32
-  attestation_record.attester_bitfield = b'\xff'*32
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
-  attestation_record.aggregate_sig.append(18446744073709551615)
+  attestation_record.slot = helpers.MAX_U64
+  attestation_record.shard_id = helpers.MAX_U64
+  attestation_record.shard_block_hash = helpers.MAX_BYTES
+  attestation_record.attester_bitfield = helpers.MAX_BYTES
+  for i in range(0, 64):
+    attestation_record.aggregate_sig.append(helpers.MAX_U64)
 
   block = packedmessages_pb2.PackedBlock()
-  block.parent_hash= b'\xff'*32
-  block.slot_number= 18446744073709551615
-  block.randao_reveal= b'\xff'
-  block.pow_chain_ref = b'\xff'*32
-  block.active_state_hash = b'\xff'*32
-  block.crystallized_state_root = b'\xff'*32
+  block.parent_hash= helpers.MAX_BYTES
+  block.slot_number= helpers.MAX_U64
+  block.randao_reveal= helpers.MAX_BYTES
+  block.pow_chain_ref = helpers.MAX_BYTES
+  block.active_state_hash = helpers.MAX_BYTES
+  block.crystallized_state_root = helpers.MAX_BYTES
+  for i in range(0, 2000):
+    arecord = block.attestations.add()
+    arecord.slot = helpers.MAX_U64
+    arecord.shard_id = helpers.MAX_U64
+    arecord.shard_block_hash = helpers.MAX_BYTES
+    arecord.attester_bitfield = helpers.MAX_BYTES
+    for i in range(0, 64):
+      arecord.aggregate_sig.append(helpers.MAX_U64)
+
 
   shard_and_committee = packedmessages_pb2.PackedShardAndCommittee()
-  shard_and_committee.shard_id = 18446744073709551615
+  shard_and_committee.shard_id = helpers.MAX_U64
   for i in range(0, 1000):
-    shard_and_committee.committee.append(4294967295)
+    shard_and_committee.committee.append(helpers.MAX_U32)
 
   crosslink_record = packedmessages_pb2.PackedCrosslinkRecord()
-  crosslink_record.dynasty = 18446744073709551615
-  crosslink_record.blockhash = b'\xff'*32
+  crosslink_record.dynasty = helpers.MAX_U64
+  crosslink_record.blockhash = helpers.MAX_BYTES
 
   validator_record = packedmessages_pb2.PackedValidatorRecord()
-  validator_record.public_key = 18446744073709551615
-  validator_record.withdrawal_shard = 18446744073709551615
-  validator_record.withdrawal_address = b'\xff'*32
-  validator_record.randao_commitment = b'\xff'*32
-  validator_record.balance = 18446744073709551615
-  validator_record.start_dynasty = 18446744073709551615
-  validator_record.end_dynasty = 18446744073709551615
+  validator_record.public_key = helpers.MAX_U64
+  validator_record.withdrawal_shard = helpers.MAX_U16
+  validator_record.withdrawal_address = helpers.MAX_BYTES
+  validator_record.randao_commitment = helpers.MAX_BYTES
+  validator_record.balance = helpers.MAX_U64
+  validator_record.start_dynasty = helpers.MAX_U64
+  validator_record.end_dynasty = helpers.MAX_U64
 
   crystallized_state = packedmessages_pb2.PackedCrystallizedState()
-  crystallized_state.last_state_recalc = 18446744073709551615
-  crystallized_state.justified_streak = 18446744073709551615
-  crystallized_state.last_justified_slot = 18446744073709551615
-  crystallized_state.last_finalized_slot = 18446744073709551615
-  crystallized_state.current_dynasty = 18446744073709551615
-  crystallized_state.crosslinking_start_shard = 18446744073709551615
-  crystallized_state.total_deposits = 18446744073709551615
-  crystallized_state.dynasty_seed = b'\xff'*32
-  crystallized_state.dynasty_seed_last_reset = 18446744073709551615
+  crystallized_state.last_state_recalc = helpers.MAX_U64
+  crystallized_state.justified_streak = helpers.MAX_U64
+  crystallized_state.last_justified_slot = helpers.MAX_U64
+  crystallized_state.last_finalized_slot = helpers.MAX_U64
+  crystallized_state.current_dynasty = helpers.MAX_U64
+  crystallized_state.crosslinking_start_shard = helpers.MAX_U16
+  crystallized_state.total_deposits = helpers.MAX_U64
+  crystallized_state.dynasty_seed = helpers.MAX_BYTES
+  crystallized_state.dynasty_seed_last_reset = helpers.MAX_U64
 
 
   for i in range(0, 1000):
     crecord = crystallized_state.crosslink_records.add()
-    crecord.dynasty = 18446744073709551615
-    crecord.blockhash = b'\xff'*32
+    crecord.dynasty = helpers.MAX_U64
+    crecord.blockhash = helpers.MAX_BYTES
 
   for i in range(0, 1000):
     vrecord = crystallized_state.validators.add()
-    vrecord.public_key = 18446744073709551615
-    vrecord.withdrawal_shard = 18446744073709551615
-    vrecord.withdrawal_address = b'\xff'*32
-    vrecord.randao_commitment = b'\xff'*32
-    vrecord.balance = 18446744073709551615
-    vrecord.start_dynasty = 18446744073709551615
-    vrecord.end_dynasty = 18446744073709551615
+    vrecord.public_key = helpers.MAX_U64
+    vrecord.withdrawal_shard = helpers.MAX_U64
+    vrecord.withdrawal_address = helpers.MAX_BYTES
+    vrecord.randao_commitment = helpers.MAX_BYTES
+    vrecord.balance = helpers.MAX_U64
+    vrecord.start_dynasty = helpers.MAX_U64
+    vrecord.end_dynasty = helpers.MAX_U64
 
 
   for i in range(0, 1000):
     shard_comittee_array = crystallized_state.indices_for_heights.add()
     for i in range(0, 10):
       scr = shard_comittee_array.array_shard_and_committee.add()
-      scr.shard_id = 18446744073709551615
+      scr.shard_id = helpers.MAX_U64
       for i in range(0, 1000):
-        scr.committee.append(4294967295)
+        scr.committee.append(helpers.MAX_U32)
+
 
 
   attestation_record_bytes = attestation_record.SerializeToString()
